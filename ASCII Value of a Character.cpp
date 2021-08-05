613f0df1dfbb5624368a62618c87f34870add28f
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-08-05 12:21:40 +0530

    To print ASCII Value of a Character.

---
+++ ASCII Value of a Character.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    int i;
+    cout<<"Enter the Character: ";
+    cin>>ch;
+    i = ch;
+    cout<<"\nASCII Value = "<<i;
+    cout<<"\n";
+    return 0;
+}
